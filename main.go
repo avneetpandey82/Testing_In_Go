2cc4d1a2336ed6a880889a336036dea8f794f4c7
avneet2005 <avneet@madvit.com>
2024-11-15 14:18:59 -0500

    basic functions

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func Add(a, b int) int {
+	return a + b
+}
+func Sub(a, b int) int {
+	return a - b
+}
+func Mul(a, b int) int {
+	return a * b
+}
+func main() {
+	fmt.Println(`Addition: `, Add(2024, 132143))
+	fmt.Println(`Subtraction: `, Add(2, 2))
+	fmt.Println(`Multiplication: `, Mul(10, 30))
+}
